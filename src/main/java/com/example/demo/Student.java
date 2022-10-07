13ddad31fd4d474506e2f9814de63068fdb20856
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-07 21:08:44 +0100

    @Many To Many mappedBy

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -75,6 +75,23 @@ public class Student {
     )
     private List<Book> books = new ArrayList<>();
 
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE}
+    )
+    @JoinTable(
+            name = "enrolment",
+            joinColumns = @JoinColumn(
+                    name = "student_id",
+                    foreignKey = @ForeignKey(name = "enrolment_student_id_fk")
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "course_id",
+                    foreignKey = @ForeignKey(name = "enrolment_course_id_fk")
+
+            )
+    )
+    private List<Course> courses = new ArrayList<>();
+
     public Student(String firstName,
                    String lastName,
                    String email,
