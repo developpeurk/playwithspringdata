ffab080961eab094dca2c4408ef328fda58288c2
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-07 22:13:01 +0100

    finishing many to many

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -46,12 +46,27 @@ public class Application {
 
             student.setStudentIdCard(studentIdCard);
 
-            student.enrolToCourse(new Course("Computer Science","IT"));
-            student.enrolToCourse(new Course("Amigoscode Spring Data JPA","IT"));
+          //  studentIdCardRepository.save(studentIdCard);
+
+
+
+
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L, 1L),
+                    student,
+                    new Course("Computer Science","IT"),
+                    LocalDateTime.now()
+            ));
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L, 2L),
+                    student,
+                    new Course("Amigoscode Spring Data JPA","IT"),
+                    LocalDateTime.now().minusDays(18)
+            ));
 
             studentRepository.save(student);
-            studentIdCardRepository.save(studentIdCard);
-
 
             studentRepository.findById(1L)
                     .ifPresent(s -> {
--- src/main/java/com/example/demo/Enrolment.java
+++ src/main/java/com/example/demo/Enrolment.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity(name = "Enrolment")
 @Table(name = "enrolment")
@@ -11,17 +12,47 @@ public class Enrolment {
 
     @ManyToOne
     @MapsId("studentId")
-    @JoinColumn(name = "student_id")
+    @JoinColumn(
+            name = "student_id",
+            foreignKey = @ForeignKey(
+                    name = "enrolment_student_id_fk"
+            )
+    )
     private Student student;
 
     @ManyToOne
     @MapsId("courseId")
-    @JoinColumn(name = "course_id")
+    @JoinColumn(
+            name = "course_id",
+            foreignKey = @ForeignKey(
+                    name = "enrolment_course_id_fk"
+            )
+    )
     private Course course;
 
-    public Enrolment(Student student, Course course) {
+    @Column(
+            name = "created_at",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime createdAt;
+
+    public Enrolment(EnrolmentId enrolmentId,
+                     Student student,
+                     Course course,
+                    LocalDateTime createdAt) {
+        this.enrolmentId = enrolmentId;
         this.student = student;
         this.course = course;
+        this.createdAt = createdAt;
+    }
+
+    public Enrolment(Student student,
+                     Course course,
+                     LocalDateTime createdAt) {
+        this.student = student;
+        this.course = course;
+        this.createdAt = createdAt;
     }
 
     public Enrolment() {
@@ -49,5 +80,13 @@ public class Enrolment {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
