700bdb4717c8adaa3f71775483f5acb8266f042d
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-07 14:38:17 +0100

    testing functionalities

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -17,27 +17,27 @@ public class Application {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+    CommandLineRunner commandLineRunner(
+            StudentRepository studentRepository,
+            StudentIdCardRepository studentIdCardRepository
+            ){
         return args -> {
-            generateRandomStudents(studentRepository);
-           // Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
-           // sorting(studentRepository);
-            PageRequest pageRequest = PageRequest.of(
-                    0,
-                    5,
-                    Sort.by( "firstName").ascending());
-            Page<Student> page = studentRepository.findAll(pageRequest);
-            System.out.println(page);
+            Faker faker = new Faker();
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+
+            StudentIdCard studentIdCard =
+                    new StudentIdCard(
+                            "123456789",
+                             student);
+            studentIdCardRepository.save(studentIdCard);
         };
-    }
-
-    private void sorting(StudentRepository studentRepository) {
-        Sort sort = Sort.by( "firstName").ascending()
-                .and(Sort.by("age").descending());
-
-        studentRepository.findAll(sort)
-                .forEach(student ->
-                        System.out.println(student.getFirstName() + " " + student.getAge()));
     }
 
     private void generateRandomStudents(StudentRepository studentRepository) {
